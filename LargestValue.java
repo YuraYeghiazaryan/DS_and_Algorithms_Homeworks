d5b9d8a269dccb7112d1756afd63695e0cca9bb1
YuraYeghiazaryan <101919335+YuraYeghiazaryan@users.noreply.github.com>
2023-04-25 12:56:29 +0400

    Update LargestValue.java

--- LargestValue.java
+++ LargestValue.java
@@ -6,6 +6,7 @@ public class LargestValue {
         LargestValue largeValue = new LargestValue();
         int max = largeValue.find(arr, arr.length);
         System.out.println(max);
+        System.out.println(largeValue.find1(arr, 0, arr.length-1));
 
     }
     public int find(int[] arr, int length){
@@ -13,5 +14,15 @@ public class LargestValue {
             return arr[0];
         }
         return Math.max(arr[length - 1], find(arr, length - 1));
+    }
+    public  int find1(int[] arr, int leftIndex, int rightIndex){
+        if(leftIndex == rightIndex){
+            return arr[rightIndex];
+        }
+        int mid = (leftIndex + rightIndex) / 2;
+
+        int max1 = find1(arr, leftIndex, mid);
+        int max2 = find1(arr, mid+1, rightIndex);
+        return Math.max(max1, max2);
     }
 }
